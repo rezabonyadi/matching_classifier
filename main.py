8e096deea05e0208290e17d7aeb60271d8fb4e84
rezabonyadi <Reza@1983>
2019-01-01 13:11:03 +1000

    m

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matchingclassifier.MatchingClassifier1D import MatchingClassifier1D
 from matchingclassifier.MatchingClassifier import MatchingClassifier
 from sklearn import metrics
 
@@ -9,60 +8,32 @@ from sklearn import metrics
 # from keras import backend as K
 # import keras
 
+#
+# X = np.random.rand(100)
+# X = np.concatenate((X, np.random.rand(100)+4.1), axis=0)
+# X = np.concatenate((X, np.random.rand(100)+5), axis=0)
+#
+# c = np.zeros(100)
+# c = np.concatenate((c, np.ones(100)), axis=0)
+# c = np.concatenate((c, np.ones(100)*2), axis=0)
+#
+# X = np.reshape(X, (-1, 1))
+# c = np.reshape(c, (-1, 1))
+#
+# mc = MatchingClassifier1D()
+# mc.fit_1D(X, c)
+#
+# y = mc.predict(X)
+# print(metrics.accuracy_score(y, c))
 
-X = np.random.rand(100)
-X = np.concatenate((X, np.random.rand(100)+2), axis=0)
-X = np.concatenate((X, np.random.rand(100)+5), axis=0)
+from sklearn.datasets import load_digits, load_breast_cancer
+# x, y = load_digits(2, True)
+x, y = load_breast_cancer(True)
 
-c = np.zeros(100)
-c = np.concatenate((c, np.ones(100)), axis=0)
-c = np.concatenate((c, np.ones(100)*2), axis=0)
+z = MatchingClassifier()
+z.fit(x, y)
+z.visualize_tree()
 
-X = np.reshape(X, (-1, 1))
-c = np.reshape(c, (-1, 1))
-
-mc = MatchingClassifier1D()
-mc.fit_1D(X, c)
-
-y = mc.predict(X)
-print(metrics.accuracy_score(y, c))
-
-# X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
-
-#
-# # input image dimensions
-# img_rows, img_cols = 28, 28
-# num_classes = 10
-#
-# # the data, split between train and test sets
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-#
-# if K.image_data_format() == 'channels_first':
-#     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-#     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-#     input_shape = (1, img_rows, img_cols)
-# else:
-#     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-#     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-#     input_shape = (img_rows, img_cols, 1)
-#
-# x_train = x_train[0:1000, :, :, :]
-# y_train = y_train[0:1000]
-#
-# x_train = x_train.astype('float32')
-# x_test = x_test.astype('float32')
-# x_train /= 255
-# x_test /= 255
-# print('x_train shape:', x_train.shape)
-# print(x_train.shape[0], 'train samples')
-# print(x_test.shape[0], 'test samples')
-#
-# # convert class vectors to binary class matrices
-# # y_train = keras.utils.to_categorical(y_train, num_classes)
-# # y_test = keras.utils.to_categorical(y_test, num_classes)
-#
-# X = np.reshape(x_train, (x_train.shape[0], x_train[0].size))
-#
-# mc = MatchingClassifier()
-# mc.fit(X, y_train)
-# #
+cs = z.predict(x)
+print(metrics.roc_auc_score(y, cs))
+xx=0
--- matchingclassifier/MatchingClassifier.py
+++ matchingclassifier/MatchingClassifier.py
@@ -1,48 +1,199 @@
-from matchingclassifier.MatchingClassifier1D import MatchingClassifier1D
 import numpy as np
-# from sklearn import metrics
+
 
 class MatchingClassifier:
-
-    def __init__(self):
-        pass
-
+    def __init__(self, max_depth=100):
+        self.max_depth = max_depth
+        self.tree_root = None
 
     def fit(self, X, classes):
-        number_of_variables = X.shape[1]
-        prediction_votes = np.zeros(classes.shape[0])
-        mc1d = MatchingClassifier1D()
-        performances = np.zeros(number_of_variables)
-        for i in range(number_of_variables):
-            d = X[:, i]
-            mc1d.fit_1D(d, classes)
-            c_hat = mc1d.predict(d)
-            # performances[i] = metrics.accuracy_score(c_hat, classes)
+        self.tree_root = self.build_tree(X, classes, 0, self.max_depth)
 
-            # diff = c_hat - classes
+    def visualize_tree(self):
+        self.recursive_visualize_tree(self.tree_root, 0)
 
-            # a, b = self.pick_one_dimension(X, classes, [], prediction_votes)
-        zz = 0
-    def pick_one_dimension(self, X, classes, currently_picked: list, prediction_votes):
-        mc1d = MatchingClassifier1D()
+    def recursive_visualize_tree(self, root, l):
+        if root.class_label is not None:
+            t = ''
+            for i in range(l):
+                t += '   '
+            t += ''.join(['Class is ', str(root.class_label)])
+            print(t)
 
-        temp_prediction_votes = np.zeros(classes.shape[0])
-        best_perf = 0
-        for i in range(X.shape[1]):
-            d = X[:, i]
-            mc1d.fit_1D(d, classes)
-            c_hat = mc1d.predict(d)
-            diff = c_hat - classes
-            correct = diff == 0
-            incorrect = diff != 0
-            temp_prediction_votes[correct] = prediction_votes[correct] + 1
-            temp_prediction_votes[incorrect] = prediction_votes[incorrect] - 1
+        else:
+            t = ''
+            for i in range(l):
+                t += '   '
+            t += ''.join(['if x[', str(root.selected_variable), '] <= ', str(root.threshold), ' then '])
+            print(t)
+            self.recursive_visualize_tree(root.left_node, l+1)
+            t = ''
+            for i in range(l):
+                t += '   '
+            t += ''.join('else:')
+            print(t)
+            self.recursive_visualize_tree(root.right_node, l + 1)
 
-            perf = sum(temp_prediction_votes > 0) / classes.shape[0]
-            if (best_perf > perf):
+    def predict(self, X):
+        labels = np.zeros(X.shape[0])
+        for i in range(X.shape[0]):
+            root = self.tree_root
+            x = X[i, :]
+            while root.class_label is None:
+                v = x[root.selected_variable]
+                if v <= root.threshold:
+                    root = root.left_node
+                else:
+                    root = root.right_node
+
+            labels[i] = root.class_label
+        return labels
+
+    def build_tree(self, x, y, current_depth, max_depth):
+        n1 = sum(y)
+        n0 = y.shape[0] - n1
+        perc_1 = n1 / (n0 + n1)
+        perc_0 = n0 / (n0 + n1)
+
+        if (current_depth == max_depth) | (y.std() == 0.0):
+            if perc_0 > perc_1:
+                label = 0
+            else:
+                label = 1
+            node = DecomposerTree(None, None, None, perc_0 + perc_1, perc_0, perc_1, y.shape[0], label)
+            return node
+        else:
+            best_perf, best_dimension, best_thr, best_coef, Lx, Rx, Ly, Ry, _, _ = self.pick_best_dimension(x, y)
+            node = DecomposerTree(best_thr, best_coef, best_dimension, best_perf, perc_0, perc_1, y.shape[0], None)
+            node.left_node = self.build_tree(Lx, Ly, current_depth + 1, max_depth)
+            node.right_node = self.build_tree(Rx, Ry, current_depth + 1, max_depth)
+            return node
+
+    def pick_best_dimension(self, X, Y):
+        [m, n] = X.shape
+        best_perf = -1
+        best_dimension = 0
+        best_thr = 0.0
+        t1 = np.sum(Y)
+        t0 = m - t1
+
+        for i in range(n):
+            if np.var(X[:, i]) == 0:  # Nothing to distinguish
+                continue
+            [thr, coef, perf, marg, per_l0, per_l1] = self.__optimal_discrimination__(X[:, i], Y, t0, t1)
+            if (perf > best_perf):
                 best_perf = perf
-                best_index = i
+                best_thr = thr
+                best_coef = coef
+                best_marg = marg
+                best_per_l0 = per_l0
+                best_per_l1 = per_l1
+                best_dimension = i
 
-        return best_index, best_perf
+        left_indices = np.where(X[:, best_dimension] <= best_thr)[0]
+        right_indices = np.where(X[:, best_dimension] > best_thr)[0]
 
+        LX = X[left_indices, :]
+        RX = X[right_indices, :]
+        LY = Y[left_indices]
+        RY = Y[right_indices]
 
+        return best_perf, best_dimension, best_thr, best_coef, LX, RX, LY, RY, best_per_l0, best_per_l1
+
+    @staticmethod
+    def __optimal_discrimination__(d, c, t0, t1):
+        '''
+
+        :param d: 1D data
+        :param c: classes (must be 0/1)
+        :param t0: number of instances in class 0
+        :param t1: number of instances in class 1
+        :return: thr: where to cut, coef: which direction, perf: how good it was, marg: what was the final margin
+        '''
+        d_i = d.argsort()
+        ds = d[d_i]
+        cs = c[d_i]
+
+        n1ls = 0.0
+        n0ls = 0.0
+        perf = 0.0
+
+        for i in range(cs.shape[0] - 1):
+            if cs[i] == 1:
+                n1ls += 1
+            else:
+                n0ls += 1
+
+            if ds[i] == ds[i+1]:
+                continue
+
+            l0 = (n0ls / t0)
+            l1 = (n1ls / t1)
+            acc1 = l0 + (1.0 - l1)  # Accuracy, assuming 0s to the left
+            acc2 = l1 + (1.0 - l0)  # Accuracy assuming 1s to the left
+
+            if acc1 > perf:  # 0s to the left
+                perf = acc1
+                thr = (ds[i] + ds[i+1])/2.0
+                coef = 1
+                marg = abs(ds[i]-ds[i+1])
+                per_l0 = l0
+                per_l1 = l1
+
+            if acc2 > perf: # 1s to the left
+                perf = acc2
+                thr = (ds[i] + ds[i + 1]) / 2.0
+                coef = -1
+                marg = abs(ds[i] - ds[i + 1])
+                per_l0 = l0
+                per_l1 = l1
+
+        #
+        # n1ls = np.cumsum(cs) # Number of 1s to the left of each value, inclusive
+        # n0ls = np.cumsum(-(cs - 1.0)) # Number of 0s to the left of each value, inclusive
+        # # Fix for repetitive values
+        # for i in range(d_i.shape[0] - 1, 0, -1):
+        #     if ds[i] == ds[i - 1]:
+        #         n0ls[i-1] = n0ls[i]
+        #         n1ls[i-1] = n1ls[i]
+        #
+        # l0 = np.divide(n0ls, t0)  # Percentage of 0s to the left, corresponding to items in ds
+        # l1 = np.divide(n1ls, t1)  # Percentage of 1s to the left, corresponding to items in ds
+        # acc1 = l0 + (1.0 - l1)  # Average performance, assuming 0s to the left and 1s to the right
+        # acc2 = 2.0 - acc1  # l1 + (1 - l0): Average performance, asusuming 1s to the left and 0s to the right
+        # ind1 = np.argmax(acc1) - 1  # Assuming 0s to the left, the index of the best item in ds that achieves the best performance
+        # ind2 = np.argmax(acc2) - 1  # Assuming 1s to the left, the index of the best item in ds that achieves the best performance
+        # a1 = acc1[ind1]
+        # a2 = acc2[ind2]
+        # if a1 > a2:  # 0s to the left, inclusinve
+        #     thr = ((ds[ind1] + ds[ind1 + 1]) / 2.0)
+        #     coef = 1
+        #     perf = a1
+        #     per_l0 = l0[ind1]
+        #     per_l1 = l1[ind1]
+        #     marg = abs(ds[ind1] - ds[ind1 + 1])
+        #     tind = ind1
+        # else:
+        #     thr = ((ds[ind2] + ds[ind2 + 1]) / 2.0)
+        #     coef = -1
+        #     perf = a2
+        #     per_l0 = l0[ind2]
+        #     per_l1 = l1[ind2]
+        #     marg = abs(ds[ind2] - ds[ind2 + 1])
+        #     tind = ind2
+        perf /= 2.0
+        # perf = 1 - perf
+        return thr, coef, perf, marg, per_l0, per_l1
+
+class DecomposerTree:
+    def __init__(self, t, c, d, p, l0, l1, n, l):
+        self.threshold = t # Threshold
+        self.coefficient = c # Coefficient: If negative, values <= thr are most likely to be 1s, otherwise 0s
+        self.selected_variable = d # Dimension
+        self.performance = p # Performance
+        self.percentage_of_zero = l0 # Percentage of o
+        self.percentage_of_one = l1 # Percentage of 1
+        self.number_of_instances = n # Number of instances
+        self.right_node = None # Right node
+        self.left_node = None # Left node
+        self.class_label = l
