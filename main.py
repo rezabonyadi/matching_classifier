2ebbf3ccce6e641976c4f6a0dc99aa66bb59c46c
rezabonyadi <Reza@1983>
2019-01-04 04:17:53 +1000

    m

--- main.py
+++ main.py
@@ -11,36 +11,53 @@ from sklearn.model_selection import train_test_split
 # from keras.datasets import mnist
 # (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# x, y = load_digits(2, True)
-x, y = load_breast_cancer(True)
+x, y = load_digits(10, True)
+c1 = 4
+c2 = 7
+x = x[((y==c1) | (y==c2)),:]
+y = y[((y==c1) | (y==c2))]
+y[y==c1] = 0
+y[y==c2] = 1
+
+# x, y = load_breast_cancer(True)
 
 MC = 0.0
 DT = 0.0
+nf_m = 0.0
+nf_d = 0.0
 for i in range(100):
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.5, stratify=y)
 
-    model = MatchingClassifier()
+    model = MatchingClassifier(tol=0.05, max_depth=30, extra_sparse=True)
     start_time = time.time()
     model.fit(X_train, y_train)
     print('MC time', time.time() - start_time)
-
-    # z.visualize_tree()
+    nf_m += len(model.important_dimensions)
+    print('MT #nodes', len(model.important_dimensions))
+    # model.visualize_tree()
 
     y_hat = model.predict(X_test)
-    MC += metrics.roc_auc_score(y_test, y_hat)
-    # print('MC accuracy', )
+    m = metrics.roc_auc_score(y_test, y_hat)
+    MC += m
+    print('MC accuracy', m)
 
     model = DecisionTreeClassifier()
     start_time = time.time()
     model.fit(X_train, y_train)
     print('DT time', time.time() - start_time)
+    nf_d += model.feature_importances_[model.feature_importances_ > 0].shape[0]
+    print('DT #nodes',model.feature_importances_[model.feature_importances_>0].shape[0])
     # z.visualize_tree()
 
     y_hat = model.predict(X_test)
-    DT += metrics.roc_auc_score(y_test, y_hat)
-    # print('DT accuracy', )
+    d = metrics.roc_auc_score(y_test, y_hat)
+    DT += d
+    print('DT accuracy', d)
+
 print(MC/100.0)
 print(DT/100.0)
+print(nf_m/100.0)
+print(nf_d/100.0)
 
 
 xx=0
--- matchingclassifier/MatchingClassifier.py
+++ matchingclassifier/MatchingClassifier.py
@@ -2,10 +2,12 @@ import numpy as np
 
 
 class MatchingClassifier:
-    def __init__(self, max_depth=100):
+    def __init__(self, max_depth=100, tol=0.05, extra_sparse=True):
         self.max_depth = max_depth
         self.tree_root = None
         self.important_dimensions = set()
+        self.tol = tol
+        self.extra_sparse = extra_sparse
 
     def fit(self, X, classes):
         self.tree_root = self.build_tree(X, classes, 0, self.max_depth)
@@ -27,7 +29,7 @@ class MatchingClassifier:
                 t += '   '
             t += ''.join(['if x[', str(root.selected_variable), '] <= ', str(root.threshold), ' then '])
             print(t)
-            self.recursive_visualize_tree(root.left_node, l+1)
+            self.recursive_visualize_tree(root.left_node, l + 1)
             t = ''
             for i in range(l):
                 t += '   '
@@ -64,14 +66,14 @@ class MatchingClassifier:
             node = DecomposerTree(None, None, None, perc_0 + perc_1, perc_0, perc_1, y.shape[0], label)
             return node
         else:
-            best_perf, best_dimension, best_thr, best_coef, Lx, Rx, Ly, Ry = self.pick_best_dimension(x, y)
+            best_perf, best_dimension, best_thr, best_coef, Lx, Rx, Ly, Ry = self.pick_best_dimension(x, y, self.tol)
             node = DecomposerTree(best_thr, best_coef, best_dimension, best_perf, perc_0, perc_1, y.shape[0], None)
             self.important_dimensions.add(best_dimension)
             node.left_node = self.build_tree(Lx, Ly, current_depth + 1, max_depth)
             node.right_node = self.build_tree(Rx, Ry, current_depth + 1, max_depth)
             return node
 
-    def pick_best_dimension(self, X, Y):
+    def pick_best_dimension(self, X, Y, tol):
         [m, n] = X.shape
         best_perf = -1
         best_dimension = 0
@@ -83,8 +85,9 @@ class MatchingClassifier:
             if np.var(X[:, i]) == 0:  # Cannot distinguish with no variance
                 continue
 
-            [thr, coef, perf, marg] = self.__optimal_discrimination__(X[:, i], Y, t0, t1)
-            if (perf > best_perf):
+            [thr, coef, perf, marg] = self.__optimal_discrimination__(X[:, i], Y, t0, t1, tol)
+            if ((perf > best_perf) |
+                    ((perf == best_perf) & (self.important_dimensions.__contains__(i)) & (self.extra_sparse))):
                 best_perf = perf
                 best_thr = thr
                 best_coef = coef
@@ -102,7 +105,7 @@ class MatchingClassifier:
         return best_perf, best_dimension, best_thr, best_coef, LX, RX, LY, RY
 
     @staticmethod
-    def __optimal_discrimination__(d, c, t0, t1):
+    def __optimal_discrimination__(d, c, t0, t1, m):
         '''
 
         :param d: 1D data
@@ -119,13 +122,17 @@ class MatchingClassifier:
         n0ls = 0.0
         perf = 0.0
 
+        right_set = False
+        coef = 0
+        thr = 0
+        marg = 0.0
         for i in range(cs.shape[0] - 1):
             if cs[i] == 1:
                 n1ls += 1
             else:
                 n0ls += 1
 
-            if ds[i] == ds[i+1]:
+            if ds[i] == ds[i + 1]:
                 continue
 
             l0 = (n0ls / t0)
@@ -133,18 +140,26 @@ class MatchingClassifier:
             acc1 = l0 + (1.0 - l1)  # Accuracy, assuming 0s to the left
             acc2 = l1 + (1.0 - l0)  # Accuracy assuming 1s to the left
 
-            if acc1 > perf:  # 0s to the left
+            if acc1 > perf + m:  # 0s to the left
                 perf = acc1
-                thr = (ds[i] + ds[i+1]) / 2.0
+                thr = ds[i]  # The left side of the margin
+                # thr = (ds[i] + ds[i+1]) / 2.0
                 coef = 1
-                marg = abs(ds[i] - ds[i+1])
+                marg = abs(ds[i] - ds[i + 1])
+                right_set = False
 
-            if acc2 > perf:  # 1s to the left
+            if acc2 > perf + m:  # 1s to the left
                 perf = acc2
-                thr = (ds[i] + ds[i + 1]) / 2.0
+                thr = ds[i]  # The left side of the margin
+                # thr = (ds[i] + ds[i + 1]) / 2.0
                 coef = -1
                 marg = abs(ds[i] - ds[i + 1])
+                right_set = False
 
+            if (((coef == -1) & (acc2 < perf)) | ((coef == 1) & (acc1 < perf))) & (right_set is False):
+                marg = abs(thr - ds[i])
+                thr = (thr + ds[i]) / 2.0
+                right_set = True
 
         # n1ls = np.cumsum(cs) # Number of 1s to the left of each value, inclusive
         # n0ls = np.cumsum(-(cs - 1.0)) # Number of 0s to the left of each value, inclusive
@@ -182,15 +197,16 @@ class MatchingClassifier:
         # perf = 1 - perf
         return thr, coef, perf, marg
 
+
 class DecomposerTree:
     def __init__(self, t, c, d, p, l0, l1, n, l):
-        self.threshold = t # Threshold
-        self.coefficient = c # Coefficient: If negative, values <= thr are most likely to be 1s, otherwise 0s
-        self.selected_variable = d # Dimension
-        self.performance = p # Performance
-        self.percentage_of_zero = l0 # Percentage of o
-        self.percentage_of_one = l1 # Percentage of 1
-        self.number_of_instances = n # Number of instances
-        self.right_node = None # Right node
-        self.left_node = None # Left node
+        self.threshold = t  # Threshold
+        self.coefficient = c  # Coefficient: If negative, values <= thr are most likely to be 1s, otherwise 0s
+        self.selected_variable = d  # Dimension
+        self.performance = p  # Performance
+        self.percentage_of_zero = l0  # Percentage of o
+        self.percentage_of_one = l1  # Percentage of 1
+        self.number_of_instances = n  # Number of instances
+        self.right_node = None  # Right node
+        self.left_node = None  # Left node
         self.class_label = l
